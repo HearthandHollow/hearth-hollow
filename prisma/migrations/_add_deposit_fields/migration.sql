e403185fe5924fb5249d5bc4ae0d2d989524b887
HearthandHollow <HunterHammond@thehearthhollow.com>
2026-07-06 03:20:11 -0400

    Fix: revert to prisma db push for schema synchronization

--- prisma/migrations/_add_deposit_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "estimates" ADD COLUMN "depositAmount" DOUBLE PRECISION,
-ADD COLUMN "depositPaid" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN "depositStripeSessionId" TEXT;
